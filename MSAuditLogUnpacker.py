cbdbad553cf1b7a2032431ccbb0eb94e66314c7f
William Hearn <williamhearn17@gmail.com>
2023-06-28 15:28:39 -0400

    Remove column and json arguments and preform automatically
    Remove output directory and unpack into one file

--- MSAuditLogUnpacker.py
+++ MSAuditLogUnpacker.py
@@ -1,53 +1,51 @@
+import argparse
 import pandas as pd
 import json
-import argparse
-import os
 
 
-# Function to expand JSON column
-def expand_json(data, json_cols, prefix=''):
+def expand_json(data, json_cols):
     for json_col in json_cols:
-        # Extract column
-        col = data[json_col]
-        # If the data type of the column is string, load it as JSON, else just use the dictionary
-        if data[json_col].dtype == object:
-            col = col.apply(lambda x: x if isinstance(x, dict) else json.loads(x))
-        # Get keys for JSON
-        keys = set().union(*col.apply(lambda x: list(x.keys())))
-        # Create new columns for each key
-        for key in keys:
-            data[prefix + key] = col.apply(lambda x: x.get(key))
-        data.drop(json_col, axis=1, inplace=True)
-        # Check if any new JSON columns
-        new_json_cols = [prefix + key for key in keys if isinstance(data[prefix + key].dropna().values[0], dict)]
-        for new_json_col in new_json_cols:
-            # Generate the new prefix using the current key
-            new_prefix = prefix + new_json_col.split('.')[-1] + '.'
-            data = expand_json(data, [new_json_col], prefix=new_prefix)
+        # Convert the json strings into dictionaries
+        data[json_col] = data[json_col].apply(lambda x: json.loads(x) if x is not None else {})
+        # Normalize the dictionaries into dataframes and join them with the original dataframe
+        # Specify suffixes to differentiate between overlapping columns
+        data = data.join(pd.json_normalize(data[json_col]), lsuffix="AuditData.")
+        # Drop the original json columns
+        data = data.drop(columns=json_col)
     return data
 
 
-def parse_csv_file(file_path, unique_column, json_column):
+def parse_csv_file(file_path):
     df = pd.read_csv(file_path)
-    unique_values = df[unique_column].unique()
 
-    output_dir = os.path.splitext(file_path)[0] + "_parsed"
-    os.makedirs(output_dir, exist_ok=True)
+    # Identify columns that contain JSON data
+    json_cols = [col for col in df.columns if df[col].apply(is_json).any()]
 
-    for unique_value in unique_values:
-        df_event = df[df[unique_column] == unique_value].copy()
-        df_event = expand_json(df_event, [json_column])
-        df_event.to_csv(os.path.join(output_dir, f"{unique_value}.csv"), index=False)
+    # Expand those columns
+    df = expand_json(df, json_cols)
+
+    # Save the expanded dataframe
+    output_path = file_path.replace(".csv", "_unpacked.csv")
+    df.to_csv(output_path, index=False)
+    print(f"Unpacked data saved to {output_path}.")
 
 
-# Main function
+def is_json(x):
+    if not x or type(x) != str:
+        return False
+    try:
+        json.loads(x)
+        return True
+    except:
+        return False
+
+
 def main():
-    parser = argparse.ArgumentParser(description="Parse CSV logs with JSON fields")
-    parser.add_argument('-c', type=str, required=True, help="The column to filter by for event types")
-    parser.add_argument('-j', type=str, required=True, help="The column containing JSON objects to parse")
-    parser.add_argument('-f', type=str, required=True, help="The CSV file to parse")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True, help="The path to the csv file to be processed.")
     args = parser.parse_args()
-    parse_csv_file(args.f, args.c, args.j)
+
+    parse_csv_file(args.file)
 
 
 if __name__ == "__main__":
